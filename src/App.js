924caf840cea39a5018dc8247f6db424717f724f
Mohit <mohitrakhade20@gmail.com>
2021-06-02 22:55:15 +0530

    login funtionality added

--- src/App.js
+++ src/App.js
@@ -7,16 +7,18 @@ import Play from './components/quiz/Play';
 import QuizSummary from './components/quiz/QuizSummary';
 import SignIn from './components/Registration/SignIn';
 import SignUp from './components/Registration/SignUp';
+import Sign from './components/Registration/Sign';
 
 function App() {
   return (
     <Router>
-      <Route path="/login" exact component={SignIn}/>
+      <Route path="/login" exact component={Sign}/>
       <Route path="/register" exact component={SignUp}/>
       <Route path="/play/instructions" exact component={QuizInstructions} />
       <Route path="/play/quiz" exact component={Play} />
       <Route path="/play/quizSummary" exact component={QuizSummary} />
       <Route path="/" exact component={Home}/>
+      <Route path="/test" exact component={SignIn}/>
     </Router>
   );
 }
---
+++ src/components/Registration/Sign.js
@@ -0,0 +1,181 @@
+import React, { useState } from "react";
+// import Base from "../core/Base";
+import { Redirect } from "react-router-dom";
+import "./style-cus.css"
+import { signin, authenticate, isAutheticated } from "./auth";
+// import Example from "../core/Example";
+import CssBaseline from '@material-ui/core/CssBaseline'
+import Avatar from '@material-ui/core/Avatar'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+
+import { makeStyles } from '@material-ui/core/styles';
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+const Sign = () => {
+     const classes = useStyles();
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+    error: "",
+    loading: false,
+    didRedirect: false
+  });
+
+  const { email, password, error, loading, didRedirect } = values;
+  const { user } = isAutheticated();
+
+  const handleChange = name => event => {
+    setValues({ ...values, error: false, [name]: event.target.value });
+  };
+// const performRedirect = () => {
+//     if (didRedirect) {
+//         if (user && user.role === 1) {
+//             return <Redirect to="/admin/dashboard" />;
+//         } else {
+//             return <Redirect to="/play/instructions" />
+//             // return <Redirect to="/" />;
+//       }
+//     }
+    // if (isAutheticated()) {
+    //   return <Redirect to="/" />;
+    // }
+//   };
+
+  const onSubmit = event => {
+    event.preventDefault();
+    setValues({ ...values, error: false, loading: true });
+    signin({ email, password })
+      .then(data => {
+          console.log("datda",data)
+        if (data.error) {
+          setValues({ ...values, error: data.error, loading: false });
+        } else {
+          authenticate(data, () => {
+         
+              setValues({
+                  
+                  ...values,
+                  didRedirect: true
+                })
+                if(didRedirect== 1) {window.location = "/play/instructions"} 
+                // else { window.location="/"}
+ 
+
+            });
+                
+        }
+    })
+    
+};
+
+
+//   const loadingMessage = () => {
+//     return (
+//       loading && (
+//         // <Example/>
+//         <h1>hi</h1>  
+//         )
+//     );
+//   };
+
+//   const errorMessage = () => {
+//     return (
+//       <div className="row">
+//         <div className="col-md-6 offset-sm-3 text-left">
+//           <div
+//             className="alert alert-danger"
+//             style={{ display: error ? "" : "none" }}
+//           >
+//             {error}
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   };
+
+  const signInForm = () => {
+    return (
+      <div className="row distance ">
+        <div className="col-md-6 offset-sm-3 text-left topdistance">
+
+             <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        </div>
+          <form className="centerit">
+            <div className="form-group">
+              <label className="">Email</label>
+              <input
+                onChange={handleChange("email")}
+                value={email}
+                placeholder="enter email "
+                className="form-control"
+                type="email"
+              />
+            </div>
+
+            <div className="form-group">
+              <label className="">Password</label>
+              <input
+                onChange={handleChange("password")}
+                value={password}
+                placeholder="enter password"
+                className="form-control"
+                type="password"
+              />
+            </div>
+            <button onClick={onSubmit}  className="btnit btn btn-primary btn-block btndistance">
+              Login 
+            </button>
+          </form>
+          <div className="down">
+
+              <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      CodeCat GCOEA
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    // <Base title="ADP ATLAS" >
+    <div>
+      {/* {loadingMessage()} */}
+      {/* {errorMessage()} */}
+      {signInForm()}
+      {/* { performRedirect()} */}
+</div>
+    //   {/* <p className="text-white text-center">{JSON.stringify(values)}</p> */}
+    // </Base>
+  );
+};
+
+export default Sign;
